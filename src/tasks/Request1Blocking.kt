586b323b05bdfc8f0018dc8547a18d78a2d1a0e4
seunghwan <slflfl12@naver.com>
2022-04-24 13:20:48 +0900

    2. Blocking request 🚀

--- src/tasks/Request1Blocking.kt
+++ src/tasks/Request1Blocking.kt
@@ -6,14 +6,14 @@ import retrofit2.Response
 fun loadContributorsBlocking(service: GitHubService, req: RequestData) : List<User> {
     val repos = service
         .getOrgReposCall(req.org)
-        .execute() // Executes request and blocks the current thread
+        .execute() // Executes request and blocks the current thread // Call의 메소드 현재 스레드를 멈추게함.
         .also { logRepos(req, it) }
         .body() ?: listOf()
 
     return repos.flatMap { repo ->
         service
             .getRepoContributorsCall(req.org, repo.name)
-            .execute() // Executes request and blocks the current thread
+            .execute() // Executes request and blocks the current thread //
             .also { logUsers(repo, it) }
             .bodyList()
     }.aggregate()
